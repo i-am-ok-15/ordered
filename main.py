45836232cb2907d2d2995459bdb0e61e65441c48
i-am-ok-15 <i-am-ok-15@github.com>
2025-10-05 21:37:54 +0100

    rectangles experimenation

--- main.py
+++ main.py
@@ -8,9 +8,17 @@ pygame.display.set_caption("⚔️ Ordered")
 clock = pygame.time.Clock()
 test_font = pygame.font.Font("font/Pixeltype.ttf", 70)
 
-sky_surface = pygame.image.load("graphics/Sky.png")
-ground_surface = pygame.image.load("graphics/ground.png")
+sky_surface = pygame.image.load("graphics/Sky.png").convert()
+ground_surface = pygame.image.load("graphics/ground.png").convert()
 text_surface = test_font.render("Ordered", False, "black")
+
+snail_surface = pygame.image.load("graphics/snail/snail1.png").convert_alpha()
+snail_x_pos = 800
+snail_y_pos = 300
+snail_rect = snail_surface.get_rect(midbottom = (snail_x_pos, snail_y_pos))
+
+player_surface = pygame.image.load("graphics/Player/player_walk_1.png").convert_alpha()
+player_rect = player_surface.get_rect(midbottom = (80, 300))
 
 while True:
     
@@ -22,6 +30,12 @@ while True:
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0,300))
     screen.blit(text_surface, (320, 100))
+    screen.blit(snail_surface, snail_rect)
+    snail_rect.x -= 3
+    if snail_rect.right <= 0:
+        snail_rect.left = 800
+    
+    screen.blit(player_surface, player_rect)
 
     pygame.display.update()
     clock.tick(60)
