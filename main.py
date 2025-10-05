9c5a072153b1c98d3b59430790a0d87b4e3105db
i-am-ok-15 <i-am-ok-15@github.com>
2025-10-05 20:32:41 +0100

    framerate added

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
+from sys import exit
 
 pygame.init()
 
 screen = pygame.display.set_mode((1200, 800))
+pygame.display.set_caption("⚔️ Ordered")
+clock = pygame.time.Clock()
 
 while True:
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            exit()
 
     pygame.display.update()
+    clock.tick(60)
 
